8359d51ef4594f74f9a2b4d82dd2b7d847ef3683
ssenick <ssenick@ukr.net>
2023-06-04 15:35:43 -0600

    add GetStarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
-const GetStarted = () => {
-   return (
-      <div>
-         GetStarted
+import styles from '../style';
+import {arrowUp} from  '../assets'
+const GetStarted = () => (
+      <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+         <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}></div>
       </div>
    );
-};
+
 
 export default GetStarted;
