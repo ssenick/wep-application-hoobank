23d1b8aedc25816fbe4e6af33966f4c9f429a7ba
ssenick <ssenick@ukr.net>
2023-06-05 18:55:06 -0600

    add <Testimonials/>

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {feedback} from "../constants/index.js";
+import styles from "../style.js";
+import {FeedbackCard} from "./index.js";
 
-const Testimonials = () => {
-   return (
-      <div>
-         Testimonials
+const Testimonials = () => (
+   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient opacity-[0.6]"></div>
+
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relativ z-[1]'>
+         <h2 className={styles.heading2}>What people are <br className="sm:block hidden"/> saying about us</h2>
+         <div className="w-full md:mt-0 mt-6 ">
+            <p className={`${styles.paragraph} text-left max-w-[450px]`}> Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+         </div>
       </div>
-   );
-};
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+         {feedback.map((card) => (
+            <FeedbackCard key={card.id} {...card}/>
+         ))}
+      </div>
+   </section>
+);
 
 export default Testimonials;
